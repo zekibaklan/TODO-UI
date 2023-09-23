97177ca0112a7593164fd1c980774c5a29c1f6d0
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-09-23 14:52:24 +0300

    first commit

---
+++ Todo App/View/AddTodoView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTodoView.swift
+//  Todo App
+//
+//  Created by Zeki Baklan on 23.09.2023.
+//
+
+import SwiftUI
+
+struct AddTodoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTodoView()
+}
