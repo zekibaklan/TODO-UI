b156310de3c2905539f3d7ccac1712bb61457966
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-09-24 15:41:19 +0300

    settings view / about the application

---
+++ Todo App/View/FormRowStaticView.swift
@@ -0,0 +1,18 @@
+//
+//  FormRowStaticView.swift
+//  Todo App
+//
+//  Created by Zeki Baklan on 24.09.2023.
+//
+
+import SwiftUI
+
+struct FormRowStaticView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FormRowStaticView()
+}
